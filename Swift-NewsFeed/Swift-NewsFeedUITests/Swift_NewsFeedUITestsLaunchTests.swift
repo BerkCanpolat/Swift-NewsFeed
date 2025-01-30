b85c836e0b6e7154c6371c52ce4adbc4c7b9c4e2
Berk Canpolat <canpolatberk19@gmail.com>
2025-01-30 17:05:08 +0300

    Swift News App Start

---
+++ Swift-NewsFeed/Swift-NewsFeedUITests/Swift_NewsFeedUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Swift_NewsFeedUITestsLaunchTests.swift
+//  Swift-NewsFeedUITests
+//
+//  Created by Berk Canpolat on 30.01.2025.
+//
+
+import XCTest
+
+final class Swift_NewsFeedUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
